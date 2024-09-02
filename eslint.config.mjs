d7c057d311bf5ea2264721470eca208eefdfa736
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-02 19:08:44 +0530

    feat: eslint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier";
+export default tseslint.config(
+    eslint.configs.recommended,
+    {
+        extends: [
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+            eslintConfigPrettier
+        ],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        files: ['**/*.ts'],
+        ...tseslint.configs.disableTypeChecked,
+    },
+
+    {
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-console': 'warn',
+        },
+    }
+);
