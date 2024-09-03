da6c02747b99c08105b74e62d9da7fe076d82366
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-03 19:48:43 +0530

    project init

--- src/tests/app.spec.ts
+++
@@ -1,8 +0,0 @@
-import config from "../config/config";
-
-describe("Test1", () => {
-    it("should be correct", () => {
-        expect(200).toBe(200);
-        expect(5001).toBe(Number(config.PORT));
-    })
-})
