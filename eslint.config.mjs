66e05f78f434dc60d0947e238c275e1ff80e079a
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-03 20:33:11 +0530

    formate

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,30 +4,30 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    {
-        extends: [
-            eslint.configs.recommended,
-            ...tseslint.configs.recommendedTypeChecked,
-            ...tseslint.configs.stylisticTypeChecked,
-        ],
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    {
-        files: ['**/*.js', "**/*.spec.ts"],
-        ...tseslint.configs.disableTypeChecked,
-    },
-    {
-        ignores: [
-            "jest.config.js",
-            "**/src/*.spec.ts",
-            "*.spec.ts",
-            ".gitignore",
-            "*.json",
-        ]
-    }
+  },
+  {
+    files: ['**/*.js', '**/*.spec.ts'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    ignores: [
+      'jest.config.js',
+      '**/src/*.spec.ts',
+      '*.spec.ts',
+      '.gitignore',
+      '*.json',
+    ],
+  }
 );
