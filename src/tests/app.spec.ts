18ee3edb3e64fbb49d488fbb0f8a2bdc617dfdc4
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-04 18:57:12 +0530

    app.spec.ts changed

--- src/tests/app.spec.ts
+++ src/tests/app.spec.ts
@@ -2,5 +2,8 @@ describe("test", () => {
     it("test2", () => {
         // let x = 45;
         expect(200).toBe(200)
+        expect(200).toBe(200)
+        expect(200).toBe(200)
+        expect(200).toBe(200)
     })
 })
